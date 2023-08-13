85c11a953fafbec31aa6f9e3f96b6309c8169929
perer <peter@works7ation.com>
2023-08-14 00:27:47 +0200

    created simple loggin form and created dashboard

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,11 @@
-import { stringAlternation } from '$lib/server'
+// import { stringAlternation } from '$lib/server'
+import { redirect } from "@sveltejs/kit";
 export const actions = {
-    default: async ({ request }) => {
-        const formData = await request.formData()
-        const string = formData.get('string')
-
-        let alteredString = await stringAlternation(string)
-        return { success: true, alteredString }
-    }
+  default: async ({ request, url }) => {
+    // const formData = await request.formData()
+    //         const string = formData.get('string')
+    //         let alteredString = await stringAlternation(string)
+    throw redirect(307, '/dashboard');
+    return { success: true, }
+  }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,62 @@
 <script>
+	import { enhance } from '$app/forms';
 	export let form;
-	let newString;
-	let success = false;
+	let login;
+	let password;
 
-	$: {
-		if (form && form.success) {
-			success = true;
-			setTimeout(() => {
-				success = false;
-			}, 3000);
-		}
-	}
-	$: altString = form?.alteredString;
+	let success = false;
+	//
+	// $: {
+	// 	if (form && form.success) {
+	// 		success = true;
+	// 		setTimeout(() => {
+	// 			success = false;
+	// 		}, 3000);
+	// 	}
+	// }
+	// // $: altString = form?.alteredString;
 </script>
 
-<div class="flex h-screen w-full flex-col rounded-lg bg-slate-300 p-6 shadow">
-	<div class="mb-8">
-		<div class="mb-4 text-center text-lg font-bold">Simple Form</div>
-		<div class="mb-4 text-center text-sm">Turns lowercase string into uppercase</div>
-		<form action="" method="post" class="flex flex-col">
+<div class="flex min-h-screen w-full flex-col justify-center">
+	<div class="mt-[20vh]">
+		<div class="m-4 text-center">
+			<h1 class="text-3xl font-bold">Login</h1>
+		</div>
+		<form
+			method="POST"
+			use:enhance
+			class="container mx-auto flex max-w-lg flex-col rounded-lg bg-slate-300/60 p-6 shadow"
+		>
 			<label class="font-meduim text-sm uppercase"
-				>String
+				>Login
+				<input bind:value={login} name="login" type="text" class="mb-2 w-full rounded border p-1" />
+			</label>
+			<label class="font-meduim text-sm uppercase">
+				Password
 				<input
-					bind:value={newString}
-					name="string"
-					type="text"
+					bind:value={password}
+					name="password"
+					type="password"
 					class="mb-2 w-full rounded border p-1"
 				/>
 			</label>
+			<!-- <button -->
+			<!-- 	disabled={!login && !password} -->
+			<!-- 	class={`${ -->
+			<!-- 		login && password ? 'bg-slate-900' : 'bg-slate-900/60' -->
+			<!-- 	} mx-auto mt-2 w-1/2 rounded-md p-2 text-lg font-semibold text-slate-100`}>Submit</button> -->
 			<button
-				disabled={!newString}
-				class={`${
-					newString ? 'bg-slate-900' : 'bg-slate-900/60'
-				} mx-auto mt-2 w-1/2 rounded-md p-2 text-lg font-semibold text-slate-100`}>Submit</button
+				class="
+					ml-auto mt-2 w-1/2 rounded-md bg-slate-900 p-2 text-lg font-semibold text-slate-100">Login</button
 			>
 		</form>
+
+		<!-- {#if altString} -->
+		<!-- 	<div class="mb-8 flex w-full flex-col rounded-lg bg-slate-500/60 p-6 text-center shadow"> -->
+		<!-- 		<p class="font-semiblod text-slate-900">{altString}</p> -->
+		<!-- 	</div> -->
+		<!-- {/if} -->
 	</div>
-
-	{#if altString}
-		<div class="mb-8 flex w-full flex-col rounded-lg bg-slate-500/60 p-6 text-center shadow">
-			<p class="font-semiblod text-slate-900">{altString}</p>
-		</div>
-	{/if}
-
 	<div class="mt-auto flex flex-col items-center justify-center">
 		<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
 		<p>
