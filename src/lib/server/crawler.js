7f81f4da57a08c4a5d7c402dafb299371e35b8af
AlistairStrange <marekjankovic0@gmail.com>
2023-08-19 13:44:34 +0200

    Queue (multiple scans), Dataset merging bugs fixed

--- src/lib/server/crawler.js
+++ src/lib/server/crawler.js
@@ -1,20 +1,29 @@
 // Crawlee crawler logic
 import { CheerioCrawler, Dataset } from 'crawlee';
+// Import the Apify SDK into your project
+import { Actor } from 'apify';
 
 export async function initiateCrawler(domain) {
-	// let list = [];
-	
+	// Get a config for the crawler
+	let config = getUniqueData(domain);
+
+	// Initialize the Apify actor
+	await Actor.init();
+
+	// Create new named queue and new named dataset
+	const requestQueue = await Actor.openRequestQueue(config.queueName);
+	const requestDataset = await Actor.openDataset(config.datasetName);
+
 	// CheerioCrawler crawls the web using HTTP requests
-	// and parses HTML using the Cheerio library.
 	const crawler = new CheerioCrawler({
+		requestQueue, // passing the new unique queue
+
 		// Use the requestHandler to process each of the crawled pages.
 		async requestHandler({ request, enqueueLinks }) {
 			// Save results as JSON to ./storage/datasets/default
-			await Dataset.pushData({ url: request.loadedUrl });
-			// list.push(request.loadedUrl);
+			await requestDataset.pushData({ url: request.loadedUrl });
 
-			// Extract links from the current page
-			// and add them to the crawling queue
+			// Extract links from the current page and add them to the crawling queue
 			await enqueueLinks();
 		}
 	});
@@ -23,6 +32,15 @@ export async function initiateCrawler(domain) {
 	await crawler.run([domain]);
 
 	// Finally return the list of urls
-	return Dataset.getData();
-	// return list;
+	return requestDataset.getData();
+}
+
+// Helper method to get the unique named data for queue & dataset
+function getUniqueData(domain) {
+	let timeStamp = new Date().getTime().toString();
+
+	return {
+		queueName: domain + '-' + timeStamp + '-' + 'queue',
+		datasetName: domain + '-' + timeStamp + '-' + 'dataset'
+	};
 }
