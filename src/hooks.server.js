f150a067a4f5fb04f5f112fd24672a85847ba252
peter <peter@works7ation.com>
2023-09-09 22:43:15 +0200

    removed hooks

--- src/hooks.server.js
+++
@@ -1,9 +0,0 @@
-import {language} from "$lib/store"
-export async function handle({event, resolve}) {
-  let headerLanguage = event.request.headers.get('accept-language').split(',').at(0)
-  language.set(
-    headerLanguage
-  )
-  const response = await resolve(event)
-  return response
-}
