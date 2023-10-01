98c95284d09a1ebb77f474463672349ded15f504
AlistairStrange <marekjankovic0@gmail.com>
2023-10-01 14:01:25 +0200

    Logic enhanced to store also suggestion objects

--- src/lib/firebase/updateCollection.js
+++ src/lib/firebase/updateCollection.js
@@ -23,3 +23,25 @@ export async function updateIssueDocument(config, data) {
 		issues: data
 	});
 }
+
+/**
+ * Asynchronously updates the 'suggestions' field of a specific document in a Firestore collection based on the provided configuration.
+ *
+ * @param {Object} config - Configuration object with the following properties:
+ *    @property {string} domain - The domain of the website.
+ *    @property {string} dateOfScan - The date when the website was scanned.
+ *    @property {string} urlId - The unique ID associated with a specific URL within the scanned website.
+ * @param {Object|string|Array} data - The data intended to update the 'suggestions' field in the Firestore document. This can be a string, array, or object.
+ * Note: Ensure the format of 'data' aligns with the structure expected in the 'suggestions' field.
+ *
+ * The function constructs a Firestore document path in the format:
+ * `domain/{domain}/dateofscan/{dateOfScan}/scannedurls/{urlId}`
+ * and updates the specified document's 'suggestions' field with the provided data.
+ */
+export async function updateSuggestionDocument(config, data) {
+	const pathToUrlUpdate = `domain/${config.domain}/dateofscan/${config.dateOfScan}/scannedurls/${config.urlId}`;
+
+	await updateDoc(doc(firestore, pathToUrlUpdate), {
+		suggestions: data
+	});
+}
