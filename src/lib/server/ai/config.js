54d7dc65676748113b9fe4dda2d8dd37876177e2
AlistairStrange <marekjankovic0@gmail.com>
2023-10-04 19:03:50 +0200

    New api key added

--- src/lib/server/ai/config.js
+++ src/lib/server/ai/config.js
@@ -1,17 +1,11 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-	apiKey: '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'
+	apiKey: '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'
 });
 
 function defaultConfig(messages, tokens) {
-	// return { model: 'gpt-3.5-turbo', messages: messages, temperature: 0.55, max_tokens: tokens };
-	return { model: 'gpt-3.5-turbo-16k', messages: messages, temperature: 0.35, max_tokens: tokens };
+	return { model: 'gpt-3.5-turbo-16k', messages: messages, temperature: 0.45, max_tokens: tokens }; // max_tokens -> tokens per single request
 }
 
 export { openai, defaultConfig };
-
-// const chatCompletion = await openai.chat.completions.create({
-//     messages: [{ role: "user", content: "Say this is a test" }],
-//     model: "gpt-3.5-turbo",
-// });
