fab06fdb5f8ca0d26e1997bab692a0ce9f2cea95
AlistairStrange <marekjankovic0@gmail.com>
2023-11-19 18:03:14 +0100

    Minor update of login page UI / styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,6 +5,11 @@
 	let activeTab = 'login';
 	export let form;
 
+	/**
+	 * Function to switch the active tab.
+	 *
+	 * @param {string} tab - The tab to switch to.
+	 */
 	function switchTab(tab) {
 		activeTab = tab;
 		if ((form !== undefined) & form.error) {
@@ -15,30 +20,33 @@
 
 <div class="flex min-h-screen w-full flex-col justify-center">
 	<div class="mt-[20vh]">
+		<!-- Headlines -->
 		<div class="m-4 text-center">
 			{#if activeTab === 'login'}
-				<h1 class="text-3xl font-bold">Login</h1>
+				<h1 class="text-2xl font-bold">Log-in with existing account</h1>
 			{/if}
 			{#if activeTab === 'registration'}
-				<h1 class="text-3xl font-bold">Register an account</h1>
+				<h1 class="text-2xl font-bold">Register new account</h1>
 			{/if}
 			{#if activeTab === 'resetPwd'}
-				<h1 class="text-3xl font-bold">Reset your password</h1>
+				<h1 class="text-2xl font-bold">Reset your password</h1>
 			{/if}
 		</div>
-		<div class="container mx-auto flex max-w-lg rounded-lg">
+
+		<!-- Tabs -->
+		<div class="container mx-auto flex max-w-md rounded-lg">
 			<button
 				class={activeTab === 'login'
-					? 'mt-2 inline w-full rounded-t-md border border-transparent bg-slate-400/60 px-4 py-2 text-white'
-					: 'mt-2 inline w-full rounded-t-md border border-transparent px-4 py-2 text-slate-500 hover:border-slate-400/60'}
+					? 'mt-2 inline w-full rounded-t-md border border-transparent bg-secondary px-4 py-2 text-gray-400'
+					: 'mt-2 inline w-full rounded-t-md border border-transparent px-4 py-2 text-slate-500 hover:border-secondary'}
 				on:click={() => switchTab('login')}
 			>
 				Login
 			</button>
 			<button
 				class={activeTab === 'registration'
-					? 'mt-2 inline w-full rounded-t-md border border-transparent bg-slate-400/60 px-4 py-2 text-white'
-					: 'mt-2 inline w-full rounded-t-md border border-transparent px-4 py-2 text-slate-500 hover:border-slate-400/60'}
+					? 'mt-2 inline w-full rounded-t-md border border-transparent bg-secondary px-4 py-2 text-gray-400'
+					: 'mt-2 inline w-full rounded-t-md border border-transparent px-4 py-2 text-slate-500 hover:border-secondary'}
 				on:click={() => switchTab('registration')}
 			>
 				Registration
@@ -50,22 +58,24 @@
 			method="POST"
 			action="?/login"
 			use:enhance
-			class="container mx-auto flex max-w-lg flex-col rounded-b-lg bg-slate-400/60 p-6 shadow"
+			class="container mx-auto flex max-w-md flex-col rounded-b-lg bg-secondary p-6 shadow"
 			class:hidden={activeTab !== 'login'}
 		>
-			<label class="font-meduim text-sm uppercase">
-				E-mail
-				<input bind:value={email} name="email" type="text" class="mb-2 w-full rounded border p-1" />
-			</label>
-			<label class="font-meduim text-sm uppercase">
-				Password
-				<input
-					bind:value={password}
-					name="password"
-					type="password"
-					class="mb-2 w-full rounded border p-1"
-				/>
-			</label>
+			<input
+				bind:value={email}
+				name="email"
+				type="text"
+				class="mb-2 w-full rounded border p-1 text-lg font-bold placeholder-secondary"
+				placeholder="Your e-mail"
+			/>
+
+			<input
+				bind:value={password}
+				name="password"
+				type="password"
+				class="mb-2 w-full rounded border p-1 text-lg font-bold placeholder-secondary"
+				placeholder="Your password"
+			/>
 
 			{#if form?.error}
 				<p class="text-red-500">Error: {form?.message.slice(1, -1)}</p>
@@ -77,7 +87,7 @@
 				</p>
 			{/if}
 			<button
-				class="text-md ml-auto mt-2 w-1/2 rounded-md bg-purple-500 p-2 font-semibold text-slate-100"
+				class="text-md ml-auto mt-2 w-1/2 rounded-md bg-primary p-2 font-semibold text-slate-100"
 			>
 				Login
 			</button>
@@ -88,29 +98,31 @@
 			method="POST"
 			action="?/register"
 			use:enhance
-			class="container mx-auto flex max-w-lg flex-col rounded-b-lg bg-slate-400/60 p-6 shadow"
+			class="container mx-auto flex max-w-md flex-col rounded-b-lg bg-secondary p-6 shadow"
 			class:hidden={activeTab !== 'registration'}
 		>
-			<label class="font-meduim text-sm uppercase">
-				E-mail
-				<input bind:value={email} name="email" type="text" class="mb-2 w-full rounded border p-1" />
-			</label>
-			<label class="font-meduim text-sm uppercase">
-				Password
-				<input
-					bind:value={password}
-					name="password"
-					type="password"
-					class="mb-2 w-full rounded border p-1"
-				/>
-			</label>
+			<input
+				bind:value={email}
+				name="email"
+				type="text"
+				class="mb-2 w-full rounded border p-1 text-lg font-bold placeholder-secondary"
+				placeholder="Your e-mail"
+			/>
+
+			<input
+				bind:value={password}
+				name="password"
+				type="password"
+				class="mb-2 w-full rounded border p-1 text-lg font-bold placeholder-secondary"
+				placeholder="Your password"
+			/>
 
 			{#if form?.error}
 				<p class="text-red-500">Error: {form?.message.slice(1, -1)}</p>
 			{/if}
 
 			<button
-				class="text-md ml-auto mt-2 w-1/2 rounded-md bg-purple-500 p-2 font-semibold text-slate-100"
+				class="text-md ml-auto mt-2 w-1/2 rounded-md bg-primary p-2 font-semibold text-slate-100"
 			>
 				Register
 			</button>
@@ -121,13 +133,16 @@
 			method="POST"
 			action="?/resetPassword"
 			use:enhance
-			class="container mx-auto flex max-w-lg flex-col rounded-b-lg bg-slate-400/60 p-6 shadow"
+			class="container mx-auto flex max-w-md flex-col rounded-b-lg bg-secondary p-6 shadow"
 			class:hidden={activeTab !== 'resetPwd'}
 		>
-			<label class="font-meduim text-sm uppercase">
-				E-mail
-				<input bind:value={email} name="email" type="text" class="mb-2 w-full rounded border p-1" />
-			</label>
+			<input
+				bind:value={email}
+				name="email"
+				type="text"
+				class="mb-2 w-full rounded border p-1 text-lg font-bold placeholder-secondary"
+				placeholder="Your email for resetting the password"
+			/>
 
 			{#if form?.error}
 				<p class="text-red-500">Error: {form?.message.slice(1, -1)}</p>
@@ -137,7 +152,7 @@
 			{/if}
 
 			<button
-				class="text-md ml-auto mt-2 w-1/2 rounded-md bg-purple-500 p-2 font-semibold text-slate-100"
+				class="text-md ml-auto mt-2 w-1/2 rounded-md bg-primary p-2 font-semibold text-slate-100"
 			>
 				Reset password
 			</button>
