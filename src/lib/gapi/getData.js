d13d9163d027c85670fec657d04b8dbecadc7e55
AlistairStrange <marekjankovic0@gmail.com>
2024-02-25 13:56:27 +0100

    Refactor, data are stored to FB unfiltered

--- src/lib/gapi/getData.js
+++ src/lib/gapi/getData.js
@@ -1,7 +1,7 @@
 import { gapi } from './index';
 import { getConfig } from '../utils/config';
 import { updateSearchConsoleDocument } from '../firebase/updateCollection';
-import { printLog } from '../utils/logger';
+import { filterDomainData } from './filterData';
 
 /**
  * Retrieves data from the Google Search Console API and stores it in Firebase.
@@ -34,11 +34,11 @@ export async function getData(domain, domainId, startDate, endDate, dimensions =
 
 		if (response && response.status === 200 && response.data) {
 			// Filter the data with custom algorithm
-			const perspectivePages = evaluateDomain(response.data.rows);
+			// const perspectivePages = filterDomainData(response.data.rows);
 
 			// Store the filtered data in Firebase
-			await updateSearchConsoleDocument(config, perspectivePages);
-			// Print to terminal
+			// await updateSearchConsoleDocument(config, perspectivePages);
+			await updateSearchConsoleDocument(config, response.data.rows);
 		}
 
 		// Return data to Front-end
@@ -46,22 +46,4 @@ export async function getData(domain, domainId, startDate, endDate, dimensions =
 	} catch (err) {
 		return err;
 	}
-}
-
-/**
- * Evaluates the domain data based on average impressions, average CTR, and position.
- * @param {Array} data - The array of data objects representing domain statistics.
- * @returns {Array} - The filtered data based on the evaluation criteria.
- */
-function evaluateDomain(data) {
-	// 1. Get average # of impressions of the whole domain
-	const avgImpressions = data.reduce((acc, item) => acc + item.impressions, 0) / data.length;
-	// 2. Get average CTR of the whole domain
-	const avgCtr = data.reduce((acc, item) => acc + item.ctr, 0) / data.length;
-	// 3. Get pages with above average # of impressions, below average of CTR and position lower than 5
-	const filteredData = data.filter(
-		(item) => item.impressions > avgImpressions && item.ctr < avgCtr && item.position >= 5
-	);
-
-	return filteredData;
 }
